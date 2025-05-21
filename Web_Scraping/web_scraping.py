e08750dd0d4b16f1c826d1eb49839d558246e892
Aminul-461 <aminul.physicsru@gmail.com>
2025-05-21 17:47:16 +0600

    Update web_scraping.py
    this is the modification file

--- Web_Scraping/web_scraping.py
+++ Web_Scraping/web_scraping.py
@@ -1,33 +1,33 @@
 import requests as re
 from bs4 import BeautifulSoup as bs
-# Fetch th Web page
+#Fetch th Web page
 
 # url= 'https://example.com' 
 
-# response = re.get(url)
-# print(response.status_code)
-# print(response.text)
+response = re.get(url)
+print(response.status_code)
+print(response.text)
 
 # # Parse the HTMl
-# soup= bs(response.text,'html.parser')
-# print(soup.prettify())
+soup= bs(response.text,'html.parser')
+print(soup.prettify())
 
 # #Extract data(Heading,paragraphs,Links)
  
 # # GET the first heading
-# heading=soup.find('h1')
-# print(heading.text)
+heading=soup.find('h1')
+print(heading.text)
 
 # #get all pragraph
-# paragraph=soup.find_all('p')
+paragraph=soup.find_all('p')
 
-# for para in paragraph:
-#       print(para.text)
+for para in paragraph:
+      print(para.text)
 
 # # Get all links
-# links=soup.find_all('a')
-# for link in links:
-#       print(link.get('href'))
+links=soup.find_all('a')
+for link in links:
+      print(link.get('href'))
 
 
 url='https://www.bbc.com/news'
